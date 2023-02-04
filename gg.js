2dc062277c9e495345e1b82d6adbdd347cf5a060
Mark Rozubayev <smeisiks140121mark@mail.ru>
2023-02-04 04:08:43 +0300

    initional commit

---
+++ gg.js
@@ -0,0 +1,13 @@
+let but = document.querySelector(".julia_button")
+function showAge() {
+    let age = prompt()
+    if (age >= 14 && age <= 90) {
+        alert("находится")
+        // but.style.background = 'green'
+    } else {
+        alert('не находится')
+      // but.style.background = 'purple'     
+    }
+    
+}
+but.addEventListener("click",showAge)
